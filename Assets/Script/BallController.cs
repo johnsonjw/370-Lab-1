1c9e978a42e274d78ccd1d807b782af2b84e53b2
johnsonjw <46804126+johnsonjw@users.noreply.github.com>
2019-02-01 12:22:01 -0600

    Initial commit

---
+++ Assets/Script/BallController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour 
+{
+    public float speed;
+    public float brakeStrength;
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            //Vector3 brakeVelocity = new Vector3(rb.velocity.x*);
+            //rb.velocity = new Vector3(rb.velocity.x * (1-brakeStrength), rb.velocity.y, rb.velocity.z * (1 - brakeStrength));
+            rb.angularVelocity = rb.angularVelocity * (1-brakeStrength);
+        }
+    }
+}
